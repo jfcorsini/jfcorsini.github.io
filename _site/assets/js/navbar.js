9d010133f8e16f933bf1c2d7308bc342414a694c
Joao Fernando Corsini <jf.corsini@gmail.com>
2017-06-18 17:53:19 -0300

    Remove sections that won't be used
    After some months not touching the code, I figured out that making this website like the way it was going would be boring. Too similar and boring.
    
    So I'm changing the directions :)

--- _site/assets/js/navbar.js
+++ _site/assets/js/navbar.js
@@ -1,18 +1,18 @@
 window.onload = function(){ 
-    document.querySelector("#nav-profile").onclick = function(){
-    positionOfElement = $('#profile').offset().top - $('.navbar').height(); // Position of #profile - nav height = correct position
-    $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
+    document.querySelector("#nav-about").onclick = function(){
+        positionOfElement = $('#about').offset().top - $('.navbar').height(); // Position of #about - nav height = correct position
+        $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
     };
     document.querySelector("#nav-education").onclick = function(){
-    positionOfElement = $('#education').offset().top - $('.navbar').height(); // Position of #education - nav height = correct position
-    $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
+        positionOfElement = $('#education').offset().top - $('.navbar').height(); // Position of #education - nav height = correct position
+        $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
     };
     document.querySelector("#nav-experience").onclick = function(){
-    positionOfElement = $('#experience').offset().top - $('.navbar').height(); // Position of #experience - nav height = correct position
-    $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
+        positionOfElement = $('#experience').offset().top - $('.navbar').height(); // Position of #experience - nav height = correct position
+        $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
     };
-    document.querySelector("#nav-habilities").onclick = function(){
-    positionOfElement = $('#habilities').offset().top - $('.navbar').height(); // Position of #habilities - nav height = correct position
-    $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
+    document.querySelector("#nav-abilities").onclick = function(){
+        positionOfElement = $('#abilities').offset().top - $('.navbar').height(); // Position of #abilities - nav height = correct position
+        $("html, body").animate({scrollTop:positionOfElement}, '500', 'swing');
     };
 };
