479ee328e603f8148f038603bf521cf960a322de
João Corsini <jf.corsini@gmail.com>
2025-11-13 14:21:21 +0200

    New icons

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -1,7 +1,13 @@
+import Folder from "../assets/icons/folder.svg?react";
 import Globe from "../assets/icons/globe.svg?react";
+import LockClosed from "../assets/icons/lock-closed.svg?react";
+import LockOpen from "../assets/icons/lock-open.svg?react";
 
 export const icons = {
   Globe,
+  Folder,
+  LockClosed,
+  LockOpen,
 };
 
 export type IconName = keyof typeof icons;
